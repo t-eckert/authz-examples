f672795f2f5c0fea2d5207f1a43ab57c033d7f42
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-12-26 12:52:14 -0500

    Add Gitignore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from authz-examples!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def read():
+    return {"message": "Hello, World!"}
+
